7e9983fb43165c286e28ea0c87b72e1457ec3bd8
Andrew Ezeani <ezeaniiandrew@gmail.com>
2024-07-07 11:23:53 +0100

    feat: add dummy products data

---
+++ src/data/products.js
@@ -0,0 +1,87 @@
+import productImage1 from "../assets/product-image-1.png";
+import productImage2 from "../assets/product-image-2.png";
+import productImage3 from "../assets/product-image-3.png";
+import productImage4 from "../assets/product-image-4.png";
+import productImage5 from "../assets/product-image-5.png";
+import productImage6 from "../assets/product-image-6.png";
+import productImage7 from "../assets/product-image-7.png";
+import productImage8 from "../assets/product-image-8.png";
+import productImage9 from "../assets/product-image-9.png";
+import productImage10 from "../assets/product-image-10.png";
+import productImage11 from "../assets/product-image-11.png";
+import productImage12 from "../assets/product-image-12.png";
+
+export default products = [
+  {
+    name: "cosrx oil free birch sap mostuirizer",
+    img: productImage1,
+    current_price: 16,
+    old_price: 16,
+  },
+  {
+    name: "la roche posay double repair mosturizer",
+    img: productImage2,
+    current_price: 16,
+    old_price: 16,
+  },
+  {
+    name: "minon amino moist charge mosturizer",
+    img: productImage3,
+    current_price: 16,
+    old_price: 16,
+  },
+  {
+    name: "labiotte hyalbiome water cream 1.7 fl oz",
+    img: productImage4,
+    current_price: 16,
+    old_price: 16,
+  },
+  {
+    name: "snail mucin moisturizer face cream",
+    img: productImage5,
+    current_price: 16,
+    old_price: 16,
+  },
+  {
+    name: "troubless hydro soothing face cream",
+    img: productImage6,
+    current_price: 16,
+    old_price: 16,
+  },
+  {
+    name: "olay active hydrating cream moisturizer",
+    img: productImage7,
+    current_price: 16,
+    old_price: 16,
+  },
+  {
+    name: "l'oreal paris hydra-renewal face cream",
+    img: productImage8,
+    current_price: 16,
+    old_price: 16,
+  },
+  {
+    name: "e.l.f. skin holy hydration moisturizer",
+    img: productImage9,
+    current_price: 16,
+    old_price: 16,
+  },
+  {
+    name: "wrinkle retinol regenerating face cream",
+    img: productImage10,
+    current_price: 16,
+    old_price: 16,
+  },
+  {
+    name: "cosrx snail mucin 92% moisturizer",
+    img: productImage11,
+    current_price: 16,
+    old_price: 16,
+  },
+  {
+    name: "dema pure revitalizing face cream",
+    img: productImage12,
+    current_price: 16,
+    old_price: 16,
+  },
+];
