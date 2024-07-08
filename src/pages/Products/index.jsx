b33d003697338b9eeaa33b75ad12d6eaa7758bf9
Andrew Ezeani <ezeaniiandrew@gmail.com>
2024-07-08 20:42:52 +0100

    add hero and filtering sections

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,8 +1,110 @@
+import { IoIosArrowForward } from "react-icons/io";
+import style from "./products.module.css";
+import { FiSliders } from "react-icons/fi";
+import { ProductList, Select } from "../../components";
+import { TbArrowsSort } from "react-icons/tb";
+import productsData from "../../data";
+
 function Products() {
   return (
-    <div>
-      <p>Products</p>
-    </div>
+    <>
+      <div className={style.hero}>
+        <div className={style.textBox}>
+          <h1>face moisturizers</h1>
+          <p>
+            Unleash the full potential of your skin with our curated selection
+            of targeted face serums. These lightweight, fast-absorbing formulas
+            are packed with potent active ingredients that address specific
+            concerns like fine lines, wrinkles, hyperpigmentation, and dullness.{" "}
+          </p>
+        </div>
+        <div className={style.route}>
+          <p>Home</p>
+          <span>
+            <IoIosArrowForward />
+          </span>
+          <p>Categories</p>
+          <span>
+            <IoIosArrowForward />
+          </span>
+          <p>Face moisturizers</p>
+        </div>
+      </div>
+      <div className={style.filterBox}>
+        <div className={style.box1}>
+          <div>
+            <span>
+              <FiSliders />
+            </span>
+            filter
+          </div>
+          <p>clear (2)</p>
+        </div>
+        <div className={style.box2}>
+          <Select placeholder="Price" options={["hello", "world"]} />
+          <Select
+            placeholder="Brands"
+            options={[
+              "Cerave",
+              "Naturium",
+              "Timeless",
+              "TIAM",
+              "cosrx",
+              "kojvit",
+              "The Ordinary",
+              "Acwell",
+              "Good Molecules",
+              "La Roche Posay",
+            ]}
+          />
+          <Select
+            placeholder="Skin Concerns"
+            options={[
+              "Acne",
+              "Wrinkles",
+              "Dark Spots",
+              "Hyper pigmentation",
+              "Melasma",
+              "Dry Skin",
+              "Strawberry Skin",
+            ]}
+          />
+          <Select
+            placeholder="Ingredients"
+            options={[
+              "Azelaic acid",
+              "Nacinamide",
+              "Kojk Acid",
+              "Green Tea",
+              "Tranaxemic acid",
+              "Vit. C",
+              "Snail Mucin",
+              "Hyaluronic acid",
+              "Propolis",
+              "Alpha Arbutin",
+            ]}
+          />
+        </div>
+        <div className={style.box3}>
+          <div>
+            <p>Show</p>
+            <Select placeholder={12} options={[12, 6]} />
+          </div>
+          <div>
+            <p>
+              <span>
+                <TbArrowsSort />
+              </span>
+              Sort
+            </p>
+            <Select placeholder={"Bestsellers"} options={[12, 6]} />
+          </div>
+        </div>
+      </div>
+      <div className={style.products}>
+        <ProductList products={productsData} />
+      </div>
+    </>
   );
 }
 
