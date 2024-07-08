f8a2cc9c78cc50120aaf748ffb905fdb61d010e8
Andrew Ezeani <ezeaniiandrew@gmail.com>
2024-07-08 20:49:53 +0100

    rename file

--- src/data/index.js
+++ src/data/index.js
@@ -11,77 +11,91 @@ import productImage10 from "../assets/product-image-10.png";
 import productImage11 from "../assets/product-image-11.png";
 import productImage12 from "../assets/product-image-12.png";
 
-export default products = [
+const productsData = [
   {
+    id: 1,
     name: "cosrx oil free birch sap mostuirizer",
     img: productImage1,
     current_price: 16,
     old_price: 16,
   },
   {
+    id: 2,
     name: "la roche posay double repair mosturizer",
     img: productImage2,
     current_price: 16,
     old_price: 16,
   },
   {
+    id: 3,
     name: "minon amino moist charge mosturizer",
     img: productImage3,
     current_price: 16,
     old_price: 16,
   },
   {
+    id: 4,
     name: "labiotte hyalbiome water cream 1.7 fl oz",
     img: productImage4,
     current_price: 16,
     old_price: 16,
   },
   {
+    id: 5,
     name: "snail mucin moisturizer face cream",
     img: productImage5,
     current_price: 16,
     old_price: 16,
   },
   {
+    id: 6,
     name: "troubless hydro soothing face cream",
     img: productImage6,
     current_price: 16,
     old_price: 16,
   },
   {
+    id: 7,
     name: "olay active hydrating cream moisturizer",
     img: productImage7,
     current_price: 16,
     old_price: 16,
   },
   {
+    id: 8,
     name: "l'oreal paris hydra-renewal face cream",
     img: productImage8,
     current_price: 16,
     old_price: 16,
   },
   {
+    id: 9,
     name: "e.l.f. skin holy hydration moisturizer",
     img: productImage9,
     current_price: 16,
     old_price: 16,
   },
   {
+    id: 10,
     name: "wrinkle retinol regenerating face cream",
     img: productImage10,
     current_price: 16,
     old_price: 16,
   },
   {
+    id: 11,
     name: "cosrx snail mucin 92% moisturizer",
     img: productImage11,
     current_price: 16,
     old_price: 16,
   },
   {
+    id: 12,
     name: "dema pure revitalizing face cream",
     img: productImage12,
     current_price: 16,
     old_price: 16,
   },
 ];
+
+export default productsData;
