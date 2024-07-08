75f2dbfeadbff9e3207ac8107276b6fd865305dc
Andrew Ezeani <ezeaniiandrew@gmail.com>
2024-07-08 11:05:51 +0100

    updated base styles and app layout

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { Nav } from "../components";
+import style from "./layout.module.css";
 
 function Layout() {
   return (
     <>
-      <header>
-        <p>nav</p>
+      <header className={style.header}>
+        <Nav />
       </header>
       <main>
         <Outlet />
