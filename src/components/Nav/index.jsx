1d3e2bfa2010ea0c96ac0f77e0a31d9f70531dcd
Andrew Ezeani <ezeaniiandrew@gmail.com>
2024-07-08 11:56:33 +0100

    add logo image

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -6,28 +6,19 @@ import { MdFavoriteBorder } from "react-icons/md";
 import { BsPerson } from "react-icons/bs";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import style from "./nav.module.css";
+import logo from "../../assets/logo.png";
 
 function Nav() {
   return (
     <div className={style.mainHeader}>
       <TextField leftIcon={<IoIosSearch />} />
-      <h1>
-        <span>Bare</span>Beauty
-      </h1>
+      <div>
+        <img src={logo} alt="" />
+      </div>
       <div className={style.icons}>
         <IconWrapper icon={<IoCartOutline />} />
         <IconWrapper icon={<MdFavoriteBorder />} />
         <IconWrapper icon={<BsPerson />} />
-
-        <div className={style.language}>
-          <div>
-            <span className={style.flag}>{/* <img src="" alt="" /> */}</span>
-            <span>en</span>
-          </div>
-          <span>
-            <RiArrowDropDownLine />
-          </span>
-        </div>
       </div>
     </div>
   );
